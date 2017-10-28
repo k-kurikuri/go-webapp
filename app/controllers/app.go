a69247297665f4ee93fa5d50dc5db9b1ac8d660e
k-kurikuri <kurimixgoo@gmail.com>
2017-10-29 03:06:40 +0900

    revel tutorial sample action implements

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -9,5 +9,21 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	greeting := "Aloha World"
+	return c.Render(greeting)
+}
+
+// validation sample Action
+func (c App) Hello(myName string) revel.Result {
+	c.Validation.Required(myName).Message("Your name is required!")
+	c.Validation.MinSize(myName, 3).Message("Your name is not long enough!")
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+
+		return c.Render(App.Index)
+	}
+
+	return c.Render(myName)
 }
