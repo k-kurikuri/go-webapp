a4b3942779616703b164792f4f262db038aa9d60
k-kurikuri <kurimixgoo@gmail.com>
2017-11-06 23:12:59 +0900

    add  columns for auth

--- app/models/user.go
+++ app/models/user.go
@@ -1,11 +1,18 @@
 package models
 
-import "time"
+import (
+	"github.com/revel/modules/auth/driver/secret"
+	_ "golang.org/x/crypto/bcrypt"
+	"time"
+)
 
 type User struct {
 	Id        uint   `gorm:"primary_key"`
 	Name      string `gorm:"size:255"`
-	Password  string `gorm:"size:255"`
+	Email     string `gorm:"size:255"`
+	HashPass  string `gorm:"size:255"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
+
+	secret.BcryptAuth `sql:"-"`
 }
