fa5f029c4bf93058b59d59dee781f9c8c2425587
k-kurikuri <kurimixgoo@gmail.com>
2017-11-14 23:57:11 +0900

    認証Controllerでのemail, passwordのチェック処理を実装

--- app/controllers/auth_controller.go
+++ app/controllers/auth_controller.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"github.com/k-kurikuri/gogo-done/app/auth"
+	"github.com/k-kurikuri/gogo-done/app/db"
+	"github.com/k-kurikuri/gogo-done/app/models"
 	"github.com/revel/revel"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthController struct {
@@ -9,9 +13,30 @@ type AuthController struct {
 }
 
 func (c AuthController) Authenticate() revel.Result {
+	email := c.Params.Form.Get("email")
+	password := c.Params.Form.Get("password")
+
+	con := db.Connection()
+	var user models.User
+	con.First(&user, "email = ?", email)
+	if user.Email == "" {
+		panic("non exist user")
+	}
+
+	hashPassword, err := auth.Crypt(password)
+	if err != nil {
+		panic("fail password crypt")
+	}
+
+	// TODO: hashとパスワードの判定がおかしい
+	err = bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte(password))
+	if err != nil {
+		panic("password wrong")
+	}
+
 	// this is sample json
 	response := make(map[string]interface{})
-	response["user"] = "authUser"
-
+	response["success"] = true
+	response["user"] = user
 	return c.RenderJSON(response)
 }
