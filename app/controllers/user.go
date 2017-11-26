faa2ee891af894cd24191c97e905a82e35a652ad
k-kurikuri <kurimixgoo@gmail.com>
2017-11-26 14:51:45 +0900

    簡易的なvalidationをActionで行うよう対応

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -13,5 +13,26 @@ func (c User) Create() revel.Result {
 }
 
 func (c User) Register() revel.Result {
+	email := c.Params.Form.Get("email")
+	password := c.Params.Form.Get("password")
+	name := c.Params.Form.Get("name")
+
+	c.validate(email, password, name)
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(User.Create)
+	}
+
 	return c.RenderHTML("Register")
+}
+
+func (c User) validate(email, password, name string) {
+	c.Validation.Email(email).Message("Please format email")
+
+	c.Validation.MinSize(password, 8).Message("Password must be at least 8 characters")
+	c.Validation.MaxSize(password, 16).Message("Password must be 16 characters or less")
+
+	c.Validation.Required(name).Message("must be user name")
 }
