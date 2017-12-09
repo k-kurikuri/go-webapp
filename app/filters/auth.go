0da1644182fbcb17122807a0e67765cb38a2e144
k-kurikuri <kurimixgoo@gmail.com>
2017-12-09 23:02:43 +0900

    認証ユーザー名をviewで出力するよう対応

--- app/filters/auth.go
+++ app/filters/auth.go
@@ -1,14 +1,22 @@
 package filters
 
 import (
+	"encoding/json"
+	"github.com/k-kurikuri/gogo-done/app/models"
 	"github.com/revel/revel"
 )
 
 var AuthFilter = func(c *revel.Controller, fc []revel.Filter) {
-	var user string = c.Session["user"]
-	if user == "" {
+	var userStr string = c.Session["user"]
+	if userStr == "" {
 		panic("not authenticated")
 	}
 
 	fc[0](c, fc[1:]) // Execute the next filter stage.
+
+	user := models.User{}
+
+	json.Unmarshal([]byte(userStr), &user)
+
+	c.Render(user)
 }
