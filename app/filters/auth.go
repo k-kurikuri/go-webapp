46e314cd7d7ac10bd2c99b3a58d796beaa35b27d
k-kurikuri <kurimixgoo@gmail.com>
2017-12-10 14:35:16 +0900

    認証外アクセスはtopにリダイレクトするよう対応

--- app/filters/auth.go
+++ app/filters/auth.go
@@ -3,13 +3,15 @@ package filters
 import (
 	"encoding/json"
 	"github.com/k-kurikuri/gogo-done/app/models"
+	"github.com/k-kurikuri/gogo-done/app/routes"
 	"github.com/revel/revel"
 )
 
 var AuthFilter = func(c *revel.Controller, fc []revel.Filter) {
 	var userStr string = c.Session["user"]
 	if userStr == "" {
-		panic("not authenticated")
+		c.Result = c.Redirect(routes.Auth.Index())
+		return
 	}
 
 	fc[0](c, fc[1:]) // Execute the next filter stage.
